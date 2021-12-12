d57e5e80128991ebb7c4b7af53c1e34be90a8520
Paulo-Lopes-Estevao <pl1745240@gmail.com>
2021-12-12 15:29:23 +0100

    test(usecase/park): registe new park not actived passed: TestNewParkNotActivedLowerLimit

--- usecase/park_usecase_test.go
+++ usecase/park_usecase_test.go
@@ -36,3 +36,31 @@ func TestNewParkActived(t *testing.T) {
 	assert.Equal(t, outputExpected, outputPark)
 
 }
+
+func TestNewParkNotActivedLowerLimit(t *testing.T) {
+	input := usecase.ParkDtoInput{
+		Name:  "Viana Park",
+		Limit: 4,
+		Vague: 15,
+	}
+
+	outputExpected := usecase.ParkDtoOutput{
+		Status:       false,
+		ErrorMessage: "very lower limit",
+	}
+
+	controller := gomock.NewController(t)
+	defer controller.Finish()
+
+	repositoryMockPark := repository_mock.NewMockParkRepository(controller)
+	repositoryMockPark.EXPECT().
+		Registre(input.Name, input.Limit, input.Vague, outputExpected.Status).
+		Return(nil)
+
+	usecasePark := usecase.NewParkUsecase(repositoryMockPark)
+
+	outputPark, err := usecasePark.RegistreNewPark(input)
+	assert.Nil(t, err, err)
+	assert.Equal(t, outputExpected, outputPark)
+
+}
