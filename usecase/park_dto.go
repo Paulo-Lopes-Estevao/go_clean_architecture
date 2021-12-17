4d9fefb72afc74e4ca3a791de5bcb5bdc17ade9c
Paulo-Lopes-Estevao <pl1745240@gmail.com>
2021-12-17 13:30:11 +0100

    feat(usecase/dto): add tag json park dto

--- usecase/park_dto.go
+++ usecase/park_dto.go
@@ -1,15 +1,15 @@
 package usecase
 
 type ParkDtoInput struct {
-	Name  string
-	Vague int32
-	Limit int32
-	Type  string
-	Price float64
-	Time  int32
+	Name  string  `json:"name"`
+	Vague int32   `json:"vague"`
+	Limit int32   `json:"limit"`
+	Type  string  `json:"type"`
+	Price float64 `json:"price"`
+	Time  int32   `json:"time"`
 }
 
 type ParkDtoOutput struct {
-	Status       bool
-	ErrorMessage string
+	Status       bool   `json:"status"`
+	ErrorMessage string `json:"error_message"`
 }
