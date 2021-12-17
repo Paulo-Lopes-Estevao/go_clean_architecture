2a79696ad704080ef36485ec7da1749155613204
Paulo-Lopes-Estevao <pl1745240@gmail.com>
2021-12-17 13:29:31 +0100

    feat(interface/presenter): edit Park presenter

--- interface/park/presenter/parkRest_presenter.go
+++ interface/park/presenter/parkRest_presenter.go
@@ -1,10 +1,8 @@
 package presenter
 
-type ParkRestPresenter struct {
-	Name  string  `json:"name"`
-	Vague int32   `json:"vague"`
-	Limit int32   `json:"limit"`
-	Type  string  `json:"type"`
-	Price float64 `json:"price"`
-	Time  int32   `json:"time"`
+type ParkRestPresenterContext interface {
+	JSON(code int, i interface{}) error
+	Bind(i interface{}) error
+	Param(name string) string
+	ResponseData(map[string]interface{}) interface{}
 }
