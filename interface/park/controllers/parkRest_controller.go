249166694bbd7ad4d6059f76150a9ad9917a8445
Paulo-Lopes-Estevao <pl1745240@gmail.com>
2021-12-17 13:29:02 +0100

    feat(interface/controller): add Park Controller use create park

---
+++ interface/park/controllers/parkRest_controller.go
@@ -0,0 +1,32 @@
+package controllers
+
+import (
+	"errors"
+	"net/http"
+
+	"github.com/Paulo-Lopes-Estevao/go_clean_architecture/entities/repository"
+	"github.com/Paulo-Lopes-Estevao/go_clean_architecture/interface/park/presenter"
+	"github.com/Paulo-Lopes-Estevao/go_clean_architecture/usecase"
+)
+
+type ParkController struct {
+	parkUsecase usecase.ParkUsecaseInterface
+	repository  repository.ParkRepositoryInterface
+}
+
+func NewParkController(parkUsecase usecase.ParkUsecaseInterface) *ParkController {
+	return &ParkController{parkUsecase: parkUsecase}
+}
+
+func (c *ParkController) AddPark(ctx presenter.ParkRestPresenterContext) error {
+	input := &usecase.ParkDtoInput{}
+
+	if err := ctx.Bind(input); !errors.Is(err, nil) {
+		ctx.JSON(http.StatusBadRequest, ctx.ResponseData(map[string]interface{}{"error": err.Error()}))
+	}
+
+	usecasepark := usecase.NewParkUsecase(c.repository)
+	output, _ := usecasepark.RegistreNewPark(*input)
+
+	return ctx.JSON(http.StatusCreated, output)
+}
