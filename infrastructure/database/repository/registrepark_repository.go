8570b8c641a472877de8b57531a4b477e371a5f5
Paulo Lopes Estevão <pl1745240@gmail.com>
2022-01-02 01:18:42 +0100

    feat: add repository registre park

---
+++ infrastructure/database/repository/registrepark_repository.go
@@ -0,0 +1,30 @@
+package repository
+
+import (
+	"github.com/Paulo-Lopes-Estevao/go_clean_architecture/infrastructure/database/models"
+	"gorm.io/gorm"
+)
+
+type repositorydb struct {
+	Db *gorm.DB
+}
+
+func NewRegistreParkRepository(db *gorm.DB) *repositorydb {
+	return &repositorydb{Db: db}
+}
+
+func (repodb *repositorydb) Registre(name string, vague int32, limit int32) error {
+
+	data := models.ParkVague{
+		Name:  name,
+		Vague: vague,
+		Limit: limit,
+	}
+
+	err := repodb.Db.Create(&data).Error
+
+	if err != nil {
+		return err
+	}
+	return nil
+}
