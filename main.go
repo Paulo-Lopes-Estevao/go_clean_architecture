08fbfb009d7227e2851b6d777fa8225af5f827d7
Paulo Lopes Estevão <pl1745240@gmail.com>
2022-01-02 23:42:34 +0100

    feat: running server

--- main.go
+++ main.go
@@ -1,8 +1,25 @@
 package main
 
+import (
+	"log"
+
+	"github.com/Paulo-Lopes-Estevao/go_clean_architecture/infrastructure/api/rest/routes"
+	"github.com/Paulo-Lopes-Estevao/go_clean_architecture/infrastructure/database"
+	"github.com/Paulo-Lopes-Estevao/go_clean_architecture/injection"
+	"github.com/labstack/echo/v4"
+	"gorm.io/gorm"
+)
+
 func main() {
 
-	//server := echo.New()
+	server := echo.New()
 
-	//router.ParkRoute(server, )
+	database.ConnectionDB()
+	i := injection.NewRegistry(&gorm.DB{})
+
+	routes.ParkRoute(server, i.NewAppController())
+
+	if err := server.Start(":8000"); err != nil {
+		log.Println("Not Running Server A...", err.Error())
+	}
 }
