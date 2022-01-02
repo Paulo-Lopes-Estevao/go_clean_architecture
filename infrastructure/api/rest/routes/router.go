7286effb4d829efafcd4d389aab4e3e9ec176c96
Paulo Lopes Estevão <pl1745240@gmail.com>
2022-01-02 23:47:02 +0100

    feat: alter to interfaces

--- infrastructure/api/rest/routes/router.go
+++ infrastructure/api/rest/routes/router.go
@@ -1,11 +1,11 @@
 package routes
 
 import (
-	"github.com/Paulo-Lopes-Estevao/go_clean_architecture/injection"
+	"github.com/Paulo-Lopes-Estevao/go_clean_architecture/injection/interfaces"
 	"github.com/labstack/echo/v4"
 )
 
-func ParkRoute(e *echo.Echo, injection injection.ControllerAdapter) *echo.Echo {
+func ParkRoute(e *echo.Echo, injection interfaces.ControllerAdapter) *echo.Echo {
 
 	e.GET("/", func(context echo.Context) error { return injection.Park.Welcome(context) })
 	e.POST("/park", func(context echo.Context) error { return injection.Park.AddPark(context) })
