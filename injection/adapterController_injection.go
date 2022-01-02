8005b1fbf8ce078901437bea1258f276cb404bf8
Paulo Lopes Estevão <pl1745240@gmail.com>
2022-01-02 23:48:30 +0100

    feat: edit adapter controller injection dependence Controllers park

--- injection/adapterController_injection.go
+++ injection/adapterController_injection.go
@@ -1,11 +1,22 @@
 package injection
 
 import (
-	"github.com/Paulo-Lopes-Estevao/go_clean_architecture/adapter/park/controllers/interfaces"
+	"github.com/Paulo-Lopes-Estevao/go_clean_architecture/adapter/park/controllers"
+	interfacesController "github.com/Paulo-Lopes-Estevao/go_clean_architecture/adapter/park/controllers/interfaces"
+	"github.com/Paulo-Lopes-Estevao/go_clean_architecture/entities/repository"
+	repo "github.com/Paulo-Lopes-Estevao/go_clean_architecture/infrastructure/database/repository"
+	"github.com/Paulo-Lopes-Estevao/go_clean_architecture/usecase"
+	"github.com/Paulo-Lopes-Estevao/go_clean_architecture/usecase/interfaces"
 )
 
-type ControllerAdapter struct {
-	Park interface {
-		interfaces.ParkControllerInterface
-	}
+func (i *repositoryAdapter) NewRestUseCase() interfaces.ParkUsecaseInterface {
+	return usecase.NewParkUsecase(i.NewRestRepository())
+}
+
+func (i *repositoryAdapter) NewRestController() interfacesController.ParkControllerInterface {
+	return controllers.NewParkController(i.NewRestUseCase())
+}
+
+func (i *repositoryAdapter) NewRestRepository() repository.ParkRepositoryInterface {
+	return repo.NewRegistreParkRepository(i.DB)
 }
