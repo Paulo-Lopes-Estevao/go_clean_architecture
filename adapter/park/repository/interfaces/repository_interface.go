367c71f4e4c419ba788e69627c545c09dc4ed144
Paulo Lopes Estevão <pl1745240@gmail.com>
2022-12-02 02:28:04 +0000

    chore: remove file in layer repository

--- adapter/park/repository/interfaces/repository_interface.go
+++
@@ -1,7 +0,0 @@
-package interfaces
-
-import "github.com/Paulo-Lopes-Estevao/go_clean_architecture/entities/repository"
-
-type RepositoryFactoryInterface interface {
-	RegistryPark() repository.ParkRepositoryInterface
-}
