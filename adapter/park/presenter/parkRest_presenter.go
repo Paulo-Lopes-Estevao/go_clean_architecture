2146baa15b501c919e4bb5c9e3f58134652a1baa
Paulo Lopes Estevão <pl1745240@gmail.com>
2022-12-02 02:27:32 +0000

    chore: remove file in layer presenter

--- adapter/park/presenter/parkRest_presenter.go
+++
@@ -1,9 +0,0 @@
-package presenter
-
-type ResponseData (map[string]interface{})
-
-type ParkRestPresenterContext interface {
-	JSON(code int, i interface{}) error
-	Bind(i interface{}) error
-	Param(name string) string
-}
