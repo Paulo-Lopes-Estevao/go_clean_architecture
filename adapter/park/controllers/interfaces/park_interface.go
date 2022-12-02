20bb9c1ff8c77bb19006483b722cd804ac1f740c
Paulo Lopes Estevão <pl1745240@gmail.com>
2022-12-02 02:27:03 +0000

    chore: remove file in layer controller

--- adapter/park/controllers/interfaces/park_interface.go
+++
@@ -1,8 +0,0 @@
-package interfaces
-
-import "github.com/Paulo-Lopes-Estevao/go_clean_architecture/adapter/park/presenter"
-
-type ParkControllerInterface interface {
-	AddPark(ctx presenter.ParkRestPresenterContext) error
-	Welcome(ctx presenter.ParkRestPresenterContext) error
-}
