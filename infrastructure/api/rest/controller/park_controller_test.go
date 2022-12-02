4d498103932f8fc4e774ced1cd6026e702a96bcc
Paulo Lopes Estevão <pl1745240@gmail.com>
2022-12-02 02:51:20 +0000

    test: e2e passed

---
+++ infrastructure/api/rest/controller/park_controller_test.go
@@ -0,0 +1,78 @@
+package controller
+
+import (
+	"bytes"
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	repository_mock "github.com/Paulo-Lopes-Estevao/go_clean_architecture/entities/repository/mock"
+	"github.com/Paulo-Lopes-Estevao/go_clean_architecture/usecase"
+	"github.com/Paulo-Lopes-Estevao/go_clean_architecture/usecase/dto"
+	"github.com/golang/mock/gomock"
+	"github.com/labstack/echo/v4"
+	"github.com/likexian/gokit/assert"
+)
+
+func TestWelcomeController(t *testing.T) {
+	e := echo.New()
+
+	req := httptest.NewRequest(http.MethodGet, "/", nil)
+	recorder := httptest.NewRecorder()
+	ctx := e.NewContext(req, recorder)
+
+	controllerMock := gomock.NewController(t)
+	defer controllerMock.Finish()
+
+	repositoryMockPark := repository_mock.NewMockParkRepository(controllerMock)
+
+	usecasepark := usecase.NewParkUsecase(repositoryMockPark)
+
+	controllerpark := NewParkController(usecasepark)
+
+	err := controllerpark.Welcome(ctx)
+	assert.Nil(t, err)
+	assert.Equal(t, http.StatusOK, recorder.Code)
+
+}
+
+func TestRegitreParkController(t *testing.T) {
+	e := echo.New()
+
+	input := dto.ParkDtoInput{
+		Name:  "Viana Park",
+		Limit: 20,
+		Vague: 15,
+	}
+
+	value, erro := json.Marshal(input)
+
+	if erro != nil {
+		assert.Nil(t, erro, "Não converteu para json")
+	}
+
+	req := httptest.NewRequest(http.MethodPost, "/park", bytes.NewBuffer(value))
+	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+	recorder := httptest.NewRecorder()
+	ctx := e.NewContext(req, recorder)
+
+	controllerMock := gomock.NewController(t)
+	defer controllerMock.Finish()
+
+	repositoryMockPark := repository_mock.NewMockParkRepository(controllerMock)
+	repositoryMockPark.EXPECT().
+		Registre(input.Name, input.Limit, input.Vague, true).
+		Return(nil)
+
+	usecasepark := usecase.NewParkUsecase(repositoryMockPark)
+
+	controllerpark := NewParkController(usecasepark)
+
+	err := controllerpark.AddPark(ctx)
+
+	if err != nil {
+		assert.Equal(t, http.StatusCreated, recorder.Code, "error status code != 201")
+	}
+
+}
